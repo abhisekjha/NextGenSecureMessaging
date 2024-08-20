1aa7ae0045ac38d0cd7ec7ea6d20f148545a61c7
Abhisek Kumar Jha <abhisekjha2020@gmail.com>
2024-08-20 11:51:17 -0500

    small update on python implementation

---
+++ test_dilithium.py
@@ -0,0 +1,20 @@
+from dilithium.src.dilithium_py.dilithium import Dilithium2
+
+# Step 1: Generate a key pair
+pk, sk = Dilithium2.keygen()
+print("Public Key:", pk)
+print("Secret Key:", sk)
+
+# Step 2: Sign a message
+msg = b"Your message signed by Dilithium"
+sig = Dilithium2.sign(sk, msg)
+print("Signature:", sig)
+
+# Step 3: Verify the signature
+is_valid = Dilithium2.verify(pk, msg, sig)
+print("Is the signature valid?", is_valid)
+
+# Step 4: Test with an altered message
+altered_msg = b"Altered message"
+is_valid = Dilithium2.verify(pk, altered_msg, sig)
+print("Is the altered message signature valid?", is_valid)
